f4e91c2659545b3b87ac878da871aafa4e4f792e
Eric Summins <esummins@gmail.com>
2015-08-15 15:43:50 -0400

    Almost working

--- app/src/main/java/bitcoin/hacktheplanet/cfb/bitcointiler/KeySubmit.java
+++ app/src/main/java/bitcoin/hacktheplanet/cfb/bitcointiler/KeySubmit.java
@@ -1,5 +1,7 @@
 package bitcoin.hacktheplanet.cfb.bitcointiler;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,30 +9,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class KeySubmit extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "bitcoin.hacktheplanet.cfb.bitcointiler.MESSAGE";
 
     Button submitButton;
     EditText publicKey;
+    Context context;
+    Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Set EditText and Button
-        publicKey = (EditText) findViewById(R.id.key_enter);
         submitButton = (Button) findViewById(R.id.key_submit_button);
 
-        //Set Button listener to submit key
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String pkey = publicKey.getText().toString();
-                if (pkey.matches("^[13][a-km-zA-HJ-NP-Z0-9]{26,33}$")) {
-
-                }
-            }
-        });
-
+        context = getApplicationContext();
+        toast = Toast.makeText(context, "Invalid Public Key", Toast.LENGTH_SHORT);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_key_submit);
@@ -58,13 +53,17 @@ public class KeySubmit extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public boolean buildQR() {
+    public boolean buildQR(View view) {
+        publicKey = (EditText) findViewById(R.id.key_enter);
         String pkey = publicKey.getText().toString();
         if (pkey.matches("^[13][a-km-zA-HJ-NP-Z0-9]{26,33}$")) {
-
+            Intent pi = new Intent(this, MainActivity.class);
+            pi.putExtra(EXTRA_MESSAGE, pkey);
+            startActivity(pi);
+            return true;
         } else {
-            
+            toast.show();
+            return false;
         }
     }
-
 }
--- app/src/main/java/bitcoin/hacktheplanet/cfb/bitcointiler/MainActivity.java
+++ app/src/main/java/bitcoin/hacktheplanet/cfb/bitcointiler/MainActivity.java
@@ -2,26 +2,85 @@ package bitcoin.hacktheplanet.cfb.bitcointiler;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.ImageView;
 import android.widget.RemoteViews;
+import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 
 import cyanogenmod.app.CMStatusBarManager;
 import cyanogenmod.app.CustomTile;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView qrImage;
+    Intent intent;
+    TextView walletid;
+    WebView qrWebview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Intent oldIntent = getIntent();
-        String message = oldIntent.getStringExtra(KeySubmit.EXTRA_MESSAGE);
+        intent = getIntent();
+        String message = intent.getStringExtra(KeySubmit.EXTRA_MESSAGE);
+
+        //Temporary example wallet
+        message = "1BmTrNt7EBcE5Wj3vBLHjvtP7L8TcXyEPS";
+
+        //walletid = (TextView) findViewById(R.id.walletID);
+        //walletid.setText(message);
+
+        //qrImage = (ImageView) findViewById(R.id.QRimage);
+        //qrImage.setImageDrawable(R.drawable.exqrcode);
+
+        //Intent oldIntent = getIntent();
+        //message = oldIntent.getStringExtra(KeySubmit.EXTRA_MESSAGE);
 
         // Create a remoteviews object
         RemoteViews contentView = new RemoteViews(getPackageName(),
                 R.layout.remote_view);
+
+        Uri thing = Uri.parse("https://" +
+                "2.info/qr?data=1Agb153xWsbqS9vt8gP4vBFKHkAchLMdSX&size=200");
+
+        //Bitmap bmp = getBitmapFromURL("https://blockchain.info/qr?data=1Agb153xWsbqS9vt8gP4vBFKHkAchLMdSX&size=200");
+        /*
+        try {
+            // generate a 150x150 QR code
+            Bitmap bm = encodeAsBitmap(message, BarcodeFormat.QR_CODE, 150, 150);
+
+            if(bm != null) image_view.setImageBitmap(bm);
+        } catch (WriterException e) { //eek }
+        */
+        // contentView.setImageViewResource(R.id.QRimage, R.drawable.exqrcode);
+
+
+
+        contentView.setImageViewUri(R.id.QRimage,thing);
+
+
+        //contentView.(R.id.QR_webview).loadUrl("https://blockchain.info/qr?data=1Agb153xWsbqS9vt8gP4vBFKHkAchLMdSX&size=200");
+        //contentView.setUri(R.id.QR_webview, "loadUrl", thing);
+        //qrWebview = (WebView) findViewById(R.id.QR_webview);
+        //qrWebview.loadUrl("https://blockchain.info/qr?data=1Agb153xWsbqS9vt8gP4vBFKHkAchLMdSX&size=200");
+        //walletid = (TextView) contentView.findViewById(R.id.walletID);
+        //walletid.setText(message);
+
+
 
         // Create intent for the onclick button
         Intent cyngnIntent = new Intent(Intent.ACTION_VIEW)
@@ -42,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Build the custom tile
         CustomTile customTile = new CustomTile.Builder(this)
-                .setLabel("Remote Style From SDK")
+                .setLabel("Wallet QR Code")
                 .setIcon(R.drawable.testimage)
                 .setExpandedStyle(remoteExpandedStyle)
                 .setOnSettingsClickIntent(new Intent(this, MainActivity.class)
@@ -79,4 +138,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
