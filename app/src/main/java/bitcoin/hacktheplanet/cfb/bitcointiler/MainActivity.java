5433181ce1ae644b38c4f697373f3388885d1ad2
Eric Summins <esummins@gmail.com>
2015-08-16 05:21:55 -0400

    final backend edit

--- app/src/main/java/bitcoin/hacktheplanet/cfb/bitcointiler/MainActivity.java
+++ app/src/main/java/bitcoin/hacktheplanet/cfb/bitcointiler/MainActivity.java
@@ -1,64 +1,44 @@
 package bitcoin.hacktheplanet.cfb.bitcointiler;
 
-import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.WriterException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
+
 import java.net.URL;
 
 import cyanogenmod.app.CMStatusBarManager;
 import cyanogenmod.app.CustomTile;
 
 public class MainActivity extends AppCompatActivity {
-
     public final static String EXTRA_MESSAGE = "bitcoin.hacktheplanet.cfb.bitcointiler.MESSAGE";
-    public final static String EXTRA_MESSAGE2 = "bitcoin.hacktheplanet.cfb.bitcointiler.MESSAGE2";
-    ImageView qrImage;
-    Intent intent;
-    TextView walletid;
-    WebView qrWebview;
+
     public String message;
     String contents;
+    SharedPreferences BlockyPrefs;
+    SharedPreferences.Editor BlockyPrefsEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        intent = getIntent();
-        message = intent.getStringExtra(KeySubmit.EXTRA_MESSAGE);
-
-        //Temporary example wallet
-        //message = "1BmTrNt7EBcE5Wj3vBLHjvtP7L8TcXyEPS";
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BlockyPrefs = getSharedPreferences("BlockyPrefs", 0);
+        message = BlockyPrefs.getString("publicKey", null);
+
+
 
         ImageView iv = (ImageView) findViewById(R.id.QRimage);
         new LoadQrCodeTask().execute(iv);
@@ -66,12 +46,10 @@ public class MainActivity extends AppCompatActivity {
 
     class LoadQrCodeTask extends AsyncTask<ImageView, Void, Boolean> {
 
-        private ImageView mImageView;
         private Bitmap mQrCodeBmp;
 
         @Override
         protected Boolean doInBackground(ImageView... params) {
-            mImageView = params[0];
             try {
                 URL thumb_u = new URL("https://blockchain.info/qr?data=" + message);
                 mQrCodeBmp = BitmapFactory.decodeStream(thumb_u.openStream());
@@ -88,11 +66,6 @@ public class MainActivity extends AppCompatActivity {
             RemoteViews contentView = new RemoteViews(getPackageName(), R.layout.remote_view);
 
             contentView.setImageViewBitmap(R.id.QRimage, mQrCodeBmp);
-
-            // Create intent for the onclick button
-            Intent cyngnIntent = new Intent(Intent.ACTION_VIEW)
-                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    .setData(Uri.parse("http://www.cyngn.com"));
 
             // contentView.setOnClickPendingIntent(R.id.qr_code, PendingIntent.getActivity(MainActivity.this, cyngnIntent));
 
@@ -144,14 +117,24 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void buildNew (View view) {
+    public void resetAll (View view) {
         CMStatusBarManager.getInstance(MainActivity.this).removeTile(10);
-        message = "";
+        BlockyPrefsEdit = BlockyPrefs.edit();
+        BlockyPrefsEdit.clear().commit();
         startActivity(new Intent(this, KeySubmit.class));
         finish();
     }
 
+    public void buildNew(View view) {
+        Intent pi = new Intent(this, SettingsActivity.class);
+        startActivity(pi);
+    }
+
     public void findQR (View view) {
+        if (BlockyPrefs.getString("guid", "").equals("") || BlockyPrefs.getString("Pw1", "").equals("")){
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        }
         try {
 
             Intent intent = new Intent("com.google.zxing.client.android.SCAN");
@@ -177,7 +160,6 @@ public class MainActivity extends AppCompatActivity {
                 contents = data.getStringExtra("SCAN_RESULT");
                 Intent pi = new Intent(this, PasswordActivity.class);
                 pi.putExtra(EXTRA_MESSAGE, contents);
-                pi.putExtra(EXTRA_MESSAGE2, message);
                 startActivity(pi);
             }
             if (resultCode == RESULT_CANCELED) {
